ec4d38eadfd4d13eb5b3b22b82794be2d1de680a
BrianKheng <briankheng63@gmail.com>
2023-01-19 22:17:43 +0700

    add: output to file, execution time

--- src/24-Card-Solver.cpp
+++ src/24-Card-Solver.cpp
@@ -4,58 +4,119 @@ using namespace std;
 
 mt19937 rnd(chrono::steady_clock::now().time_since_epoch().count());
 
+const double EPS = 1e-7;
 bool valid;
-set<string>ans;
-vector<string>cards;
-string ops[4]={"+","-","*","/"},validCards[13]={"A","2","3","4","5","6","7","8","9","10","J","Q","K"},choice,inp;
+set<string> solutions;
+vector<string> cards;
+string ops[4] = {"+", "-", "*", "/"}, validCards[13] = {"A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"}, choice, inp;
 
-double operation(double card1,double card2,string op){
-    if(op=="+")return card1+card2;
-    else if(op=="-")return card1-card2;
-    else if(op=="*")return card1*card2;
-    else if(op=="/")return card1/card2;
+void output()
+{
+    if (solutions.empty())
+    {
+        cout << "Tidak ada solusi!"
+             << "\n";
+    }
+    else
+    {
+        cout << solutions.size() << ' ' << "solusi ditemukan!"
+             << "\n"
+             << "\n";
+        for (auto it : solutions)
+            cout << it << "\n";
+    }
 }
 
-void solve(vector<string>eq){
+double operation(double c1, double c2, string op)
+{
+    if (op == "+")
+        return c1 + c2;
+    else if (op == "-")
+        return c1 - c2;
+    else if (op == "*")
+        return c1 * c2;
+    else if (op == "/")
+        return c1 / c2;
+}
+
+void solveEquation(vector<string> eq)
+{
     // Case 1: ((1 op 2) op 3) op 4
-    if(abs(operation(operation(operation(stod(eq[0]),stod(eq[2]),eq[1]),stod(eq[4]),eq[3]),stod(eq[6]),eq[5])-24)<=1e-7){
-        ans.insert("(("+eq[0]+" "+eq[1]+" "+eq[2]+") "+eq[3]+" "+eq[4]+") "+eq[5]+" "+eq[6]);
+    if (abs(operation(operation(operation(stod(eq[0]), stod(eq[2]), eq[1]), stod(eq[4]), eq[3]), stod(eq[6]), eq[5]) - 24) <= EPS)
+    {
+        solutions.insert("((" + eq[0] + " " + eq[1] + " " + eq[2] + ") " + eq[3] + " " + eq[4] + ") " + eq[5] + " " + eq[6]);
     }
     // Case 2: (1 op 2) op (3 op 4)
-    if(abs(operation(operation(stod(eq[0]),stod(eq[2]),eq[1]),operation(stod(eq[4]),stod(eq[6]),eq[5]),eq[3])-24)<=1e-7){
-        ans.insert("("+eq[0]+" "+eq[1]+" "+eq[2]+") "+eq[3]+" ("+eq[4]+" "+eq[5]+" "+eq[6]+")");
+    if (abs(operation(operation(stod(eq[0]), stod(eq[2]), eq[1]), operation(stod(eq[4]), stod(eq[6]), eq[5]), eq[3]) - 24) <= EPS)
+    {
+        solutions.insert("(" + eq[0] + " " + eq[1] + " " + eq[2] + ") " + eq[3] + " (" + eq[4] + " " + eq[5] + " " + eq[6] + ")");
     }
     // Case 3: (1 op (2 op 3)) op 4
-    if(abs(operation(operation(stod(eq[0]),operation(stod(eq[2]),stod(eq[4]),eq[3]),eq[1]),stod(eq[6]),eq[5])-24)<=1e-7){
-        ans.insert("("+eq[0]+" "+eq[1]+" ("+eq[2]+" "+eq[3]+" "+eq[4]+")) "+eq[5]+" "+eq[6]);
+    if (abs(operation(operation(stod(eq[0]), operation(stod(eq[2]), stod(eq[4]), eq[3]), eq[1]), stod(eq[6]), eq[5]) - 24) <= EPS)
+    {
+        solutions.insert("(" + eq[0] + " " + eq[1] + " (" + eq[2] + " " + eq[3] + " " + eq[4] + ")) " + eq[5] + " " + eq[6]);
     }
     // Case 4: 1 op ((2 op 3) op 4)
-    if(abs(operation(stod(eq[0]),operation(operation(stod(eq[2]),stod(eq[4]),eq[3]),stod(eq[6]),eq[5]),eq[1])-24)<=1e-7){
-        ans.insert(eq[0]+" "+eq[1]+" (("+eq[2]+" "+eq[3]+" "+eq[4]+") "+eq[5]+" "+eq[6]+")");
+    if (abs(operation(stod(eq[0]), operation(operation(stod(eq[2]), stod(eq[4]), eq[3]), stod(eq[6]), eq[5]), eq[1]) - 24) <= EPS)
+    {
+        solutions.insert(eq[0] + " " + eq[1] + " ((" + eq[2] + " " + eq[3] + " " + eq[4] + ") " + eq[5] + " " + eq[6] + ")");
     }
 }
 
-void backtrack(vector<string>eq,int idx){
-    if(idx==4){
-        solve(eq);
+void findEquation(vector<string> eq, int idx)
+{
+    if (idx == 4)
+    {
+        solveEquation(eq);
         return;
     }
-    for(auto op:ops){
-        eq.push_back(op);eq.push_back(cards[idx]);
-        backtrack(eq,idx+1);
-        eq.pop_back();eq.pop_back();
+    for (auto op : ops)
+    {
+        eq.push_back(op);
+        eq.push_back(cards[idx]);
+        findEquation(eq, idx + 1);
+        eq.pop_back();
+        eq.pop_back();
     }
 }
 
-vector<string>tokenize(string s){
-    vector<string>tokens;
-    string cur="";
-    for(auto it:s){
-        if(it==' '){
-            tokens.push_back(cur);
-            cur="";
+void tukar(string &s1, string &s2)
+{
+    string temp = s1;
+    s1 = s2;
+    s2 = temp;
+}
+
+void permutation(int l, int r)
+{
+    if (l == r)
+    {
+        findEquation({cards[0]}, 1);
+    }
+    else
+    {
+        for (int i = l; i <= r; i++)
+        {
+            tukar(*(cards.begin() + l), *(cards.begin() + i));
+            permutation(l + 1, r);
+            tukar(*(cards.begin() + l), *(cards.begin() + i));
         }
-        else cur+=it;
+    }
+}
+
+vector<string> tokenize(string s)
+{
+    vector<string> tokens;
+    string cur = "";
+    for (auto it : s)
+    {
+        if (it == ' ')
+        {
+            tokens.push_back(cur);
+            cur = "";
+        }
+        else
+            cur += it;
     }
     tokens.push_back(cur);
     return tokens;
@@ -63,70 +124,108 @@ vector<string>tokenize(string s){
 
 int main()
 {
-//    freopen("input.txt", "r", stdin);
-//    freopen("output.txt", "w", stdout);
-
     // INPUT
-    cout<<"~~~ Selamat Datang Di Permainan Kartu 24! ~~~\n\n";
+    cout << "~~~ Selamat Datang Di Permainan Kartu 24! ~~~\n\n";
 
-    cout<<"1. Input dari pengguna\n2. Input auto-generate\n\nMasukan pilihan: ";
-    getline(cin,choice);
+    cout << "1. Input dari pengguna\n2. Input auto-generate\n\nMasukan pilihan: ";
+    getline(cin, choice);
 
-    while(choice!="1"&&choice!="2"){
-        cout<<"\nMasukkan tidak sesuai!\nMasukan pilihan: ";
-        getline(cin,choice);
+    while (choice != "1" && choice != "2")
+    {
+        cout << "\nMasukan tidak sesuai!\nMasukan pilihan: ";
+        getline(cin, choice);
     }
 
     // TAKE CARD
-    if(choice=="1"){
-        do{
-            valid=true;
-            cout<<"\nPilihan Kartu:\n";
-            getline(cin,inp);
-            cards=tokenize(inp);
-            if(cards.size()==4){
-                for(int i=0;i<4;i++){
-                    if(cards[i]=="2"||cards[i]=="3"||cards[i]=="4"||cards[i]=="5"||cards[i]=="6"||
-                       cards[i]=="7"||cards[i]=="8"||cards[i]=="9"||cards[i]=="10")continue;
-                    else if(cards[i]=="A")cards[i]="1";
-                    else if(cards[i]=="J")cards[i]="11";
-                    else if(cards[i]=="Q")cards[i]="12";
-                    else if(cards[i]=="K")cards[i]="13";
-                    else valid=false;
+    if (choice == "1")
+    {
+        do
+        {
+            valid = true;
+            cout << "\nPilihan kartu:\n";
+            getline(cin, inp);
+            cards = tokenize(inp);
+            if (cards.size() == 4)
+            {
+                for (int i = 0; i < 4; i++)
+                {
+                    if (cards[i] == "2" || cards[i] == "3" || cards[i] == "4" || cards[i] == "5" || cards[i] == "6" ||
+                        cards[i] == "7" || cards[i] == "8" || cards[i] == "9" || cards[i] == "10")
+                        continue;
+                    else if (cards[i] == "A")
+                        cards[i] = "1";
+                    else if (cards[i] == "J")
+                        cards[i] = "11";
+                    else if (cards[i] == "Q")
+                        cards[i] = "12";
+                    else if (cards[i] == "K")
+                        cards[i] = "13";
+                    else
+                        valid = false;
                 }
             }
-            else valid=false;
-            if(!valid){
-                cout<<"\nMasukkan tidak sesuai!"<<endl;
+            else
+                valid = false;
+            if (!valid)
+            {
+                cout << "\nMasukan tidak sesuai!"
+                     << "\n";
             }
-        }while(!valid);
+            if (valid)
+                cout << "\n";
+        } while (!valid);
     }
-    else{
-        cout<<"\nPilihan Kartu:\n";
-        for(int i=0;i<4;i++){
-            cards[i]=validCards[rnd()%13];
-            cout<<cards[i]<<' ';
-            if(cards[i]=="A")cards[i]="1";
-            else if(cards[i]=="J")cards[i]="11";
-            else if(cards[i]=="Q")cards[i]="12";
-            else if(cards[i]=="K")cards[i]="13";
+    else
+    {
+        cout << "\nPilihan kartu:\n";
+        for (int i = 0; i < 4; i++)
+        {
+            cards.push_back(validCards[rnd() % 13]);
+            cout << cards[i] << ' ';
+            if (cards[i] == "A")
+                cards[i] = "1";
+            else if (cards[i] == "J")
+                cards[i] = "11";
+            else if (cards[i] == "Q")
+                cards[i] = "12";
+            else if (cards[i] == "K")
+                cards[i] = "13";
         }
-        cout<<endl;
+        cout << "\n\n";
     }
 
     // FIND SOLUTION
-    sort(cards.begin(),cards.end());
+    auto start = chrono::high_resolution_clock::now();
+    permutation(0, 3);
+    auto stop = chrono::high_resolution_clock::now();
 
-    do{
-        backtrack({cards[0]},1);
-    }while(next_permutation(cards.begin(),cards.end()));
+    // OUTPUT
+    output();
 
-    if(ans.empty()){
-        cout<<"\nTidak ada jawaban!"<<endl;
+    cout << "\nApakah ingin menyimpan solusi? (Y/N)\nMasukan pilihan: ";
+    getline(cin, choice);
+    while (choice != "Y" && choice != "N" && choice != "y" && choice != "n")
+    {
+        cout << "\nMasukan tidak sesuai!\nMasukan pilihan: ";
+        getline(cin, choice);
     }
-    else{
-        cout<<"\n"<<ans.size()<<' '<<"Solutions found!"<<endl<<endl;
-        for(auto it:ans)cout<<it<<endl;
+
+    if (choice == "Y" || choice == "y")
+    {
+        string fileName;
+        cout << "\nMasukan nama file keluaran: ";
+        getline(cin, fileName);
+        fileName = "../test/" + fileName + ".txt";
+        freopen(fileName.c_str(), "w", stdout);
+        output();
+        freopen("CON", "w", stdout);
     }
+
+    auto duration = chrono::duration_cast<chrono::milliseconds>(stop - start);
+    cout << "\nWaktu eksekusi program: "
+         << duration.count() << " ms\n";
+
+    cout << "\n\n~~~ Terima kasih telah menggunakan program ini! ~~~\n\n";
+
     return 0;
 }
